1be51411b7fca4931c27fcd7ac0a7fbe5959398c
Usama Bhakrani <usamabhakrani@gmail.com>
2023-08-17 22:26:14 +0500

    FetchProducts ProductsContext and ProductsReducer

--- src/reducers/products_reducer.jsx
+++ src/reducers/products_reducer.jsx
@@ -22,7 +22,31 @@ const products_reducer = (state, action) => {
       isSideBarOpen: false,
     };
   }
-  
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return {
+      ...state,
+      products_loading: true,
+    };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featuredProducts = action.payload.filter(
+      (product) => product.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products: featuredProducts,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return {
+      ...state,
+      products_loading: false,
+      products_error: true,
+    };
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
