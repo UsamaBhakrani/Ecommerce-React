4f78ac5ec31b95967694c353746c1505fc2f1b89
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-24 17:05:07 +0500

    Filter Context and Reducer Initial Loading Setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ProductsProvider>
-      <App />
+      <FilterProvider>
+        <App />
+      </FilterProvider>
     </ProductsProvider>
   </BrowserRouter>
 );
