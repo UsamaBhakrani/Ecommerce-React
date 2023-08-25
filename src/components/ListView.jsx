d59e748ccb25d76c1c05e86d4e2ec23861f4a907
Usama Bhakrani <usamabhakrani@gmail.com>
2023-08-26 00:24:12 +0500

    ListView FInalized

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -12,7 +12,10 @@ const ListView = ({ products }) => {
             <div>
               <h4>{name}</h4>
               <h5 className="price">{formatPrice(price)}</h5>
-              <p>{description}</p>
+              <p>{description.substring(0, 150)}...</p>
+              <Link to={`/products/${id}`} className="btn">
+                Details
+              </Link>
             </div>
           </article>
         );
--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { styled } from "styled-components";
+import { useFilterContext } from "../context/filter_context";
+import { BsFillGridFill, BsList } from "react-icons/bs";
 
 const Sort = () => {
-  return (
-    <div>Sort</div>
-  )
-}
+  const { filtered_products: products, grid_view } = useFilterContext();
 
-export default Sort
+  return (
+    <Wrapper>
+      <div className="btn-container">
+        <button type="button" className={grid_view ? "active" : null}>
+          <BsFillGridFill />
+        </button>
+        <button type="button">
+          <BsList className={!grid_view ? "active" : null} />
+        </button>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section``;
+
+export default Sort;
