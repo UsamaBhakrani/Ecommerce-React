01c8f3210233d2f7132fb6fa73235ffd69433a54
Mohammed Usama <usamabhakrani@gmail.com>
2023-09-01 12:34:43 +0500

    setting up cart context

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context.jsx";
+import { CartProvider } from "./context/cart_context.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ProductsProvider>
       <FilterProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </FilterProvider>
     </ProductsProvider>
   </BrowserRouter>
