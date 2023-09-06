1aefa300cce4130819c567f46157d079c3990f96
Mohammed Usama <usamabhakrani@gmail.com>
2023-09-06 19:54:52 +0500

    CartPage Structure

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,30 @@
 import { styled } from "styled-components";
 import { useCartContext } from "../context/cart_context";
 import { CartContent, Hero } from "../components";
+import { Link } from "react-router-dom";
 
 const CartPage = () => {
   const { cart } = useCartContext();
-  console.log(cart)
-  return <div>CartPage</div>;
+  if (cart.length < 1) {
+    return (
+      <Wrapper className="page-100">
+        <div className="empty">
+          <h2>Your Cart is Empty</h2>
+          <Link to="/products" className="btn">
+            Fill It
+          </Link>
+        </div>
+      </Wrapper>
+    );
+  }
+  return (
+    <main>
+      <Hero title="cart" />
+      <Wrapper className="page">
+        <CartContent />
+      </Wrapper>
+    </main>
+  );
 };
 
 const Wrapper = styled.main`
