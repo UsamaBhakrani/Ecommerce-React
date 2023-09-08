eb62cad5729e29c0150b555abc199bebd95f7e45
Mohammed Usama <usamabhakrani@gmail.com>
2023-09-08 15:19:35 +0500

    Setting up CartContent Structure and Styles

--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -3,9 +3,25 @@ import { useCartContext } from "../context/cart_context";
 import { CartColumn, CartTotal, CartItem } from "../components";
 import { Link } from "react-router-dom";
 
-
 const CartContent = () => {
-  return <div>CartContent</div>;
+  const { cart, clearCart } = useCartContext();
+  console.log(cart);
+  return (
+    <Wrapper className="section section-center">
+      <CartColumn />
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  .link-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2rem;
+  }
+`;
 
 export default CartContent;
