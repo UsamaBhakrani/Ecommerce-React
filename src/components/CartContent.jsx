22158b86b8399f555e0862175b06fa0f55609865
Mohammed Usama <usamabhakrani@gmail.com>
2023-09-08 15:24:03 +0500

    Cart Content Structure and Styles

--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -12,6 +12,20 @@ const CartContent = () => {
       {cart.map((item) => {
         return <CartItem key={item.id} {...item} />;
       })}
+      <hr />
+      <div className="link-container">
+        <Link to="/products" className="link-btn">
+          Continue Shopping
+        </Link>
+        <button
+          type="button"
+          className="link-btn clear-btn"
+          onClick={clearCart}
+        >
+          Clear Cart
+        </button>
+      </div>
+      <CartTotal />
     </Wrapper>
   );
 };
@@ -21,6 +35,23 @@ const Wrapper = styled.section`
     display: flex;
     justify-content: space-between;
     margin-top: 2rem;
+  }
+
+  .link-btn {
+    background: transparent;
+    border-color: transparent;
+    text-transform: capitalize;
+    padding: 0.25rem 0.5rem;
+    background: var(--clr-primary-5);
+    color: var(--clr-white);
+    border-radius: var(--radius);
+    letter-spacing: var(--spacing);
+    font-weight: 400;
+    cursor: pointer;
+  }
+
+  .clear-btn {
+    background: var(--clr-black);
   }
 `;
 
