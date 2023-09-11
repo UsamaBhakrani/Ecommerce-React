c4f87a9e8e85635ec5bd994555ac51088d26bf34
Mohammed Usama <usamabhakrani@gmail.com>
2023-09-11 14:37:57 +0500

    CartColumns Styling

--- src/components/CartColumn.jsx
+++ src/components/CartColumn.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { styled } from "styled-components";
 
 const CartColumn = () => {
-  return (
-    <div>CartColumn</div>
-  )
-}
+  return <Wrapper>CartColumn</Wrapper>;
+};
 
-export default CartColumn
+const Wrapper = styled.div`
+  display: none;
+
+  @media (min-width: 776px) {
+    display: block;
+
+    .content {
+      display: grid;
+      grid-template-columns: 316px 1fr 1fr 1fr auto;
+      justify-items: center;
+      column-gap: 1rem;
+      h5 {
+        color: var(--clr-grey-5);
+        font-weight: 400;
+      }
+    }
+  }
+`;
+
+export default CartColumn;
