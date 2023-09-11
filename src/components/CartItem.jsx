6fb220e302d77238a85694a7de7b7146419d6f78
Mohammed Usama <usamabhakrani@gmail.com>
2023-09-11 16:08:54 +0500

    CartItem Structure

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -4,12 +4,31 @@ import { styled } from "styled-components";
 import { AmountButtons } from "../components";
 import { FaTrash } from "react-icons/fa";
 
-const CartItem = () => {
-  return <Wrapper>CartItem</Wrapper>;
+const CartItem = ({ id, image, name, color, price, amount }) => {
+  const { removeItem, toggleAmount } = useCartContext();
+
+  const increase = () => {};
+
+  const decrease = () => {};
+
+  return (
+    <Wrapper>
+      <div className="title">
+        <img src={image} alt={name} />
+        <div>
+          <h5 className="name">{name}</h5>
+          <p className="color">
+            color: <span style={{ background: color }}></span>
+          </p>
+          <h5 className="price-small">{formatPrice(price)}</h5>
+        </div>
+      </div>
+      <h5 className="price">{formatPrice(price)}</h5>
+      <AmountButtons amount={amount} increase={increase} decrease={decrease} />
+    </Wrapper>
+  );
 };
 
-const Wrapper = styled.section `
-  
-`
+const Wrapper = styled.article``;
 
 export default CartItem;
