5768e24e808f68df0b8c777ba2748b78995c6d4b
Usama Bhakrani <usamabhakrani@gmail.com>
2023-09-15 07:45:13 +0500

    Integrating Auth0

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,26 @@ import { BrowserRouter } from "react-router-dom";
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context.jsx";
 import { CartProvider } from "./context/cart_context.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
-  </BrowserRouter>
+  <Auth0Provider
+    domain="dev-7zel80584lh3lkv7.us.auth0.com"
+    clientId="KAEUQHo5nKVVtWQj3WNu0pBUbCDApBJT"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    cacheLocation="localstorage"
+  >
+    <BrowserRouter>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </BrowserRouter>
+  </Auth0Provider>
 );
